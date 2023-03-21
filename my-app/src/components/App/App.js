74c81d271da83217b8f87bd710e6195f207f3fe5
manonGalle <manon_galle@hotmail.com>
2023-03-21 20:06:39 +0100

    build(all) : structure of app

---
+++ my-app/src/components/App/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Quote from './Quote/Quote';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Quote/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/App/Quote/Quote.js
@@ -0,0 +1,12 @@
+function Quote() {
+    return (
+      <div className="Quote">
+        Advice # COMPOSANT Advice ID goes here 
+
+        "Advice text goes here"
+      </div>
+    );
+  }
+  
+  export default Quote;
+  
