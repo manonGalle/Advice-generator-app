625b849d9dbe49acba6b409feffa4f3b3a5bfcc4
manonGalle <manon_galle@hotmail.com>
2023-03-28 22:06:46 +0200

    build(all):creation of composant and call API

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -1,13 +1,13 @@
 
-import './App.css';
+import './App.scss';
 import Quote from './Quote/Quote';
 
 function App() {
   return (
     <div className="App">
-
-      <Quote/>
-      
+      <div className='App-Quote'>
+        <Quote/>
+      </div>
     </div>
   );
 }
--- my-app/src/components/App/Quote/Quote.js
+++ my-app/src/components/App/Quote/Quote.js
@@ -1,9 +1,31 @@
+import './Quote.scss';
+import dice from '../../../assets/images/icon-dice.svg';
+import axios from 'axios';
+import { useEffect } from 'react';
+
 function Quote() {
+
+  useEffect(() => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        console.log(response)
+        const advice = response.data.slip.advice;
+        console.log(advice)
+      });
+      //.catch(error) => {
+        //console.error("error dans l'appel API")
+      //}
+  })
+
     return (
       <div className="Quote">
-        Advice # COMPOSANT Advice ID goes here 
+        <h1>Advice # COMPOSANT Advice ID goes here </h1>
 
-        "Advice text goes here"
+        <p>"Advice text goes here"</p>
+        <p>--- || ---</p>
+        <div className='Quote-circle-dice'>
+          <img src={dice} alt="dice" className='Quote-dice'></img>
+        </div>
       </div>
     );
   }
